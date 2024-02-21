5ecb8f81d15cf9d17fa73ff6db9d2a89836fa181
Josh <joshual2004.work@gmail.com>
2024-02-21 13:24:32 -0500

    added zoom to histogram

--- HTML file/modules/histogram.js
+++ HTML file/modules/histogram.js
@@ -29,7 +29,7 @@ export function loadHistogram(filter) {
   // set the dimensions and margins of the graph
   let histWidth =
     screen.width < 1024 ? (screen.width * 11) / 12 : (screen.width * 9) / 24;
-  let margin = { top: 0, right: 20, bottom: 40, left: 30 },
+  let margin = { top: 0, right: 20, bottom: 40, left: 20 },
     width = histWidth - margin.left - margin.right,
     height = (screen.height * 5) / 12 - margin.top - margin.bottom;
   d3.select("#histogram").selectAll("*").remove();
@@ -40,6 +40,7 @@ export function loadHistogram(filter) {
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
+    .call(zoom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
@@ -99,10 +100,12 @@ export function loadHistogram(filter) {
 
   // add X axis
   var x = d3.scaleBand().domain(groups).range([0, width]).padding(0.1);
+  const xAxis = d3.axisBottom(x).tickSizeOuter(0);
   svg
     .append("g")
     .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x).tickSizeOuter(0))
+    .attr("class", "x-axis")
+    .call(xAxis)
     .selectAll("text")
     .attr("text-anchor", "end")
     .attr("dx", "-2em")
@@ -135,6 +138,7 @@ export function loadHistogram(filter) {
     .attr("fill", function (d) {
       return color(d.key);
     })
+    .attr("class", "bars")
     .selectAll("rect")
     .data(function (d) {
       return d;
@@ -167,4 +171,44 @@ export function loadHistogram(filter) {
       });
       loadHistogram(d.target.__data__.data["ligand-receptor"]);
     });
+
+  function zoom(svg) {
+    const extent = [
+      [0, margin.top],
+      [width, height - margin.top],
+    ];
+
+    svg.call(
+      d3
+        .zoom()
+        .scaleExtent([1, 20])
+        .translateExtent(extent)
+        .extent(extent)
+        .on("zoom", zoomed)
+    );
+
+    function zoomed(event) {
+      x.range([0, width].map((d) => event.transform.applyX(d)));
+      svg
+        .selectAll(".bars rect")
+        .attr("x", (d) => x(d.data["ligand-receptor"]))
+        .attr("width", x.bandwidth());
+      if (event.transform.k > 10) {
+        svg
+          .selectAll(".x-axis text")
+          .attr("dx", "2em")
+          .attr("dy", "1em")
+          .attr("transform", "rotate(-20)")
+          .attr("font-size", "1em");
+      } else {
+        svg
+          .selectAll(".x-axis text")
+          .attr("dx", "-2em")
+          .attr("dy", "-2em")
+          .attr("transform", "rotate(-65)")
+          .attr("font-size", "0.3em");
+      }
+      svg.selectAll(".x-axis").call(xAxis);
+    }
+  }
 }
